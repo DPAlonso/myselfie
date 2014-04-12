f277eb27ca71e0b53a4f92a0153029cf18a64637
David <david@Davids-Mac.local>
2014-04-12 12:27:07 +0200

    Initial Commit

---
+++ myselfie/DPAAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  DPAAppDelegate.h
+//  myselfie
+//
+//  Created by David on 12/04/14.
+//  Copyright (c) 2014 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DPAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ myselfie/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  myselfie
+//
+//  Created by David on 12/04/14.
+//  Copyright (c) 2014 David. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DPAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAAppDelegate class]));
+    }
+}
